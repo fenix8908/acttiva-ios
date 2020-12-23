0d2e04cf150fd5899675243b1961fb716f530dfe
jorge <marialuci1019@gmail.com>
2020-12-23 07:56:25 -0500

    Subiendo proyecto inicial

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  constructor(public actionSheetController: ActionSheetController) { }
+
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Albums',
+      cssClass: 'my-custom-class',
+      buttons: [{
+        text: 'Ir a Cpcuite',
+        role: 'destructive',
+        icon: 'link',
+        handler: () => {
+          console.log('Delete clicked');
+          location.href="https://ionicons.com/";
+        }
+      }, 
+       {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }]
+    });
+    await actionSheet.present();
+  }
+
+
+}
